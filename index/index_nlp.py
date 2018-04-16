292bd6eeb79f218300e25c8d65f121ad6411bd56
lewis levenberg <lewis@lewislevenberg.com>
2018-04-16 16:56:37 -0400

    clean up, add comments, tighten up bigram selection

--- index/index_nlp.py
+++ index/index_nlp.py
@@ -1,5 +1,6 @@
 # Python workbook for nltk processing of Recoll index.
 
+# get the python libraries that we need
 from __future__ import division
 import nltk, re, pprint, numpy, six, matplotlib, string
 from bs4 import BeautifulSoup
@@ -16,31 +17,43 @@ stop = stopwords.words('english') + list(string.punctuation)
 index_report = open('indexed_texts_report.html')
 raw = index_report.read()
 report_cleaned = BeautifulSoup(raw).get_text()
+# break this down into individual words / terms
 tokens = word_tokenize(report_cleaned)
-cleanup = [token.lower() for token in tokens if token.lower() not in stop and len(token)>2]
 
-# term frequency distributions without and then with a stoplist
 text1 = nltk.Text(tokens)
+
+# term frequency distribution of the raw list of words
 fdist1 = nltk.FreqDist(text1)
 fdist1.plot()
 
+# remove very short words (less than 5 chars) and words not in the stoplist
+cleanup = [token.lower() for token in tokens if token.lower() not in stop and len(token)>5]
 text_clean = nltk.Text(cleanup)
+
+# term frequency distribution of the cleaned up terms
 fdist2 = nltk.FreqDist(text_clean)
 fdist2.plot()
 
-# let's count some collocations, raw and then filtered
+# let's count the 2-term collocations (bigrams), raw and then filtered
 bigram_measures = nltk.collocations.BigramAssocMeasures()
 
 finder = BigramCollocationFinder.from_words(text_clean)
+
+# sort out the most frequent raw bigrams from the cleaned text
 scored = finder.score_ngrams(bigram_measures.raw_freq)
 all_bigrams = sorted(bigram for bigram, score in scored)
+# print these to a file
 output_all_bigrams = open('all_bigrams.txt', 'w')
 print >>output_all_bigrams, all_bigrams 
 output_all_bigrams.close()
 
-finder = BigramCollocationFinder.from_words(text_clean, 5)
-finder.apply_freq_filter(5)
-filtered_bigrams = finder.nbest(bigram_measures.likelihood_ratio, 1000)
+# get the cleaned up terms' bigrams, even across intervening words
+finder = BigramCollocationFinder.from_words(text_clean, window_size = 20)
+# filter out bigrams appearing less than twice across the index
+finder.apply_freq_filter(2)
+# now select for the top 30 'most likely' bigrams
+filtered_bigrams = finder.nbest(bigram_measures.likelihood_ratio, 30)
+# print these to a file
 output_filtered_bigrams = open('filtered_bigrams.txt', 'w')
 print >>output_filtered_bigrams, filtered_bigrams 
 output_filtered_bigrams.close()
